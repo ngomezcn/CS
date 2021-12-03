7d5237f549638347141c4ff33b02c496ab9b8fc7
ngomez <naimgomezcn@gmail.com>
2021-12-03 10:23:06 +0100

    passed

---
+++ Arrays-DS.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    int N = 0;
+
+    std::cin >> N;
+
+    std::vector<int> A;
+
+    for (size_t i = 0; i < N; i++)
+    {
+        int aux = 0;
+        std::cin >> aux;
+        A.push_back(aux);
+    }
+
+    for (size_t i = N; i != 0; i--)
+    {
+        std::cout << A[i-1] << "\n";
+    }
+    
+    return 0;
+}
