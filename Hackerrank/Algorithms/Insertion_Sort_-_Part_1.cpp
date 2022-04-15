967b083f9882c1d43a4443717eb73f2d711ecb1b
ngomezcn <naimgomezcn@gmail.com>
2022-04-15 19:38:50 +0200

    Passed

--- Hackerrank/Algorithms/Insertion_Sort_-_Part_1.cpp
+++ Hackerrank/Algorithms/Insertion_Sort_-_Part_1.cpp
@@ -1,4 +1,13 @@
-int lowest = arr[arr.size() - 1];
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {2, 3, 4, 5, 6, 7, 8, 9, 10, 1};
+
+    int lowest = arr[arr.size() - 1];
     for (int i = arr.size() - 1; i >= 1; i--)
     {
         if (arr[i - 1] > lowest)
@@ -13,11 +22,17 @@ int lowest = arr[arr.size() - 1];
             cout << endl;
             break;
         }
-        if(i == 1) {
-           arr[0] = lowest; 
-        }
 
         for (int j = 0; j < arr.size(); ++j)
             cout << arr[j] << " ";
         cout << endl;
+
+        if (i == 1)
+        {
+            arr[0] = lowest;
+            for (int j = 0; j < arr.size(); ++j)
+                cout << arr[j] << " ";
+            cout << endl;
+        }
     }
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+for (int j = 0; j < arr.size(); ++j)
+            cout << arr[j] << " ";
+        cout << endl;
